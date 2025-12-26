84f95ef2c9666f8cfdc3b5e3af77734010533ea5
Alanna Tanisya Anwar <96564861+alannatnsy@users.noreply.github.com>
2025-12-26 23:52:24 +0700

    fix(data-admin): update path for list admin's datas

--- resources/js/Layouts/Admin/AppSidebar.vue
+++ resources/js/Layouts/Admin/AppSidebar.vue
@@ -175,7 +175,7 @@ const menuGroups = [
                 subItems: [
                     { name: "Anggota", path: "/anggota", pro: false },
                     { name: "Verifikasi", path: "/verifikasi", pro: false },
-                    { name: "Admin", path: "/admin", pro: false },
+                    { name: "Admin", path: "/admin/admin", pro: false },
                 ],
             },
             {
