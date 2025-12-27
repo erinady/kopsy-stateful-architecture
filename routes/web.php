32bc56326c30ddb72c4b34e8ad9514c70301e745
Alanna Tanisya Anwar <96564861+alannatnsy@users.noreply.github.com>
2025-12-28 00:27:33 +0700

    fix(data-admin): update route for list admin

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     Route::get('/users/show/{id}', [UserController::class, 'show'])->name('users.show');
 
+    Route::get('/admins', [AdminController::class, 'list']);
     Route::get('/create', [AdminController::class, 'create']);
     Route::post('/store', [AdminController::class, 'store']);
     Route::get('/show/{id}', [AdminController::class, 'show']);
