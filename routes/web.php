a19d5352d614faec33d4700581fec4e64fc095b3
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2025-12-31 11:06:05 +0700

    feat(route): add register page route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/auth/register', function () {
+    return Inertia::render('Auth/Register');
+})->name('register');
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
