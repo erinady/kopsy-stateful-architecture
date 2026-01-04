177a36cf5fc60f77e86e13233be8ff346645aa0c
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2026-01-04 23:21:00 +0700

    feat(layout): add auth layout

---
+++ resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="min-h-screen bg-no-repeat bg-center bg-cover"
+    style="background-image: url('/images/auth/bg_auth.png')"
+  >
+    <div class="min-h-screen bg-blue-900/60 flex items-center justify-center">
+      <slot />
+    </div>
+  </div>
+</template>
