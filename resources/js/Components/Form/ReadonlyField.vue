83ec4828c22a07b7f5bf21120abb0fcfb4c4eeb1
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2026-01-05 22:04:03 +0700

    feat(components): add reusable readonly field component

---
+++ resources/js/Components/Form/ReadonlyField.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+    label: {
+        type: String,
+        default: '',
+    },
+    modelValue: {
+        type: [String, Number],
+        default: '',
+    },
+    type: {
+        type: String,
+        default: 'text',
+    },
+    disabled: {
+        type: Boolean,
+        default: true,
+    },
+    placeholder: {
+        type: String,
+        default: '',
+    },
+})
+</script>
+
+<template>
+    <div class="flex flex-col gap-2">
+        <label v-if="label" class="text-sm font-medium text-gray-600">{{ label }}</label>
+        <input
+            :type="type"
+            :value="modelValue"
+            :placeholder="placeholder"
+            :disabled="disabled"
+            class="h-11 w-full rounded-lg border border-gray-200 bg-gray-50 px-4 text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        />
+    </div>
+</template>
