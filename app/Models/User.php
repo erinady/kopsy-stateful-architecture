a48bdb3926b8fcb1ada9f471fb3513aecc87a447
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2026-01-07 10:47:42 +0700

    chore(user): add member_number to fillable attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,7 @@ class User extends Authenticatable
         'dependents',
         'status',
         'joined_date',
+        'member_number',
         'email',
         'password',
         'role_id',
