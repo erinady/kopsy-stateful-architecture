cc5ddcf9fa0c7729e5f47bf71708ea50f7046529
Alanna Tanisya Anwar <96564861+alannatnsy@users.noreply.github.com>
2026-01-11 03:18:52 +0700

    feat(pengunduran-diri-anggota): add route for get and post data pengunduran diri anggota

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\SavingController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\User\AnggotaController;
+use App\Models\User;
 
 Route::get('/', function () {
     return Inertia::render('LandingPage', [
@@ -77,11 +78,14 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
 // User Routes
 Route::prefix('user')->name('user.')->middleware('auth')->group(function () {
-    Route::get('dashboard', [AnggotaController::class, 'index'])->name('userDashboard');
+    Route::get('/dashboard', [AnggotaController::class, 'index'])->name('userDashboard');
 
     Route::get('/profile/{user:member_number}', [UserController::class, 'profile'])->name('profile.show');
     Route::get('/profile/{user:member_number}/edit', [UserController::class, 'editProfile'])->name('profile.edit');
     Route::put('/profile/{user:member_number}', [UserController::class, 'updateProfile'])->name('profile.update');
     Route::post('/profile/{user:member_number}/picture', [UserController::class, 'updateProfilePicture'])->name('profile.picture.update');
     Route::delete('/profile/{user:member_number}/picture', [UserController::class, 'deleteProfilePicture'])->name('profile.picture.delete');
+
+    Route::get('/resign/{user:member_number}', [AnggotaController::class, 'createResign'])->name('resign.create');
+    Route::post('/resign/{user:member_number}', [AnggotaController::class, 'storeResign'])->name('resign.store');
 });
