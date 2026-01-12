e7e82fdce2ecbf9c14ea1ef92f3aa015b56866a0
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2026-01-12 20:30:25 +0700

    fix(component): improve dark mode styling for BaseSelect and BaseInput

--- resources/js/Components/Form/BaseSelect.vue
+++ resources/js/Components/Form/BaseSelect.vue
@@ -17,18 +17,18 @@ defineEmits(['update:modelValue'])
       @change="$emit('update:modelValue', ($event.target as HTMLSelectElement).value)"
       :disabled="disabled"
       class="peer h-12 w-full rounded-lg border border-gray-300
-             bg-transparent px-4 pt-2 pb-2 text-sm
-             text-gray-800
-             focus:border-brand-500 focus:ring-2 focus:ring-brand-500/20"
+              bg-white dark:bg-gray-800 px-4 pt-2 pb-2 text-sm
+              text-gray-800 dark:text-gray-200
+              focus:border-brand-500 focus:ring-2 focus:ring-brand-500/20"
     >
       <option value="" disabled hidden></option>
-
       <slot />
     </select>
 
     <label
       class="pointer-events-none absolute left-3 top-1/2 z-10
-             -translate-y-1/2 px-1 bg-white text-sm text-gray-400
+             -translate-y-1/2 px-1
+             bg-white dark:bg-gray-800 text-sm text-gray-400
              transition-all duration-200
              peer-focus:top-0 peer-focus:text-xs
              peer-[&:not([value=''])]:top-0
